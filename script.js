1900c6243054ca47721891b1d264cd78510eddea
Ownn01 <116043201+OWNN01@users.noreply.github.com>
2025-09-07 13:14:48 -0500

    Add in some dynamic text to set the groundwork for calcs

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const testHeader = document.querySelector(".header1");
 
-testHeader.addEventListener("click", () => { alert("testing") });
-
+// Sets up the operator functions.
 
 function add(firstNum, secondNum) {
     return firstNum + secondNum;
@@ -20,15 +18,43 @@ function divide(firstNum, secondNum) {
 }
 
 
-const displayNum1 = 0;
-const displayNum2 = 0;
-const displayOperator = "";
+function operate(operator, num1, num2) {
 
+}
+
+
+let displayNum1 = "";
+let displayNum2 = "";
+let displayOperator = "";
+
+function updateDisplay(num) {
+
+    //console.log(num.target.textContent)
+    if ((num.target.className === "numButton") && !displayOperator) {
+        displayNum1 = displayNum1 + "" + num.target.textContent;
+    } else if (num.target.className === "numButton") {
+        displayNum2 = displayNum2 + "" + num.target.textContent;
+    }
+
+    //This is broken??? TODO
+    if (num.target.classname === "operator") {
+        let opString = num.target.textContent
+        displayOperator = num.target.textContent;
+    }
+
+
+    displayFunction.textContent = displayNum1;
+}
+
+
+const display = document.querySelector(".display")
 const displayFunction = document.createElement("p");
-displayFunction.textContent = "0 + 1";
-testHeader.appendChild(displayFunction);
+displayFunction.textContent = "0";
+display.appendChild(displayFunction);
 
 
+
+// Sets up the number buttons.
 
 const numContainer = document.querySelector(".numContainer");
 
@@ -37,6 +63,7 @@ for (let i = 0; i < 3; i++) {
     let row = document.createElement("div");
     for (let i = 0; i < 3; i++) {
         let num = document.createElement("button");
+        num.className = "numButton";
         num.textContent = currentNum;
         currentNum++;
         row.appendChild(num);
@@ -44,24 +71,50 @@ for (let i = 0; i < 3; i++) {
     numContainer.appendChild(row);
 }
 
+const numButtons = document.querySelectorAll(".numButton");
+
+numButtons.forEach((e) => {
+    e.addEventListener("click", (e) => {
+        updateDisplay(e);
+    });
+});
+
+
+// sets up the operator buttons.
 
 const opContainer = document.querySelector(".operatorContainer");
 
 const ad = document.createElement("button");
+ad.className = "operator";
 ad.textContent = "+";
 opContainer.appendChild(ad);
 
 const sub = document.createElement("button");
+sub.className = "operator";
 sub.textContent = "-";
 opContainer.appendChild(sub);
 
 const mult = document.createElement("button");
+mult.className = "operator";
 mult.textContent = "*";
 opContainer.appendChild(mult);
 
 const div = document.createElement("button");
+div.className = "operator";
 div.textContent = "/";
 opContainer.appendChild(div);
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((e) => {
+    e.addEventListener("click", (e) => {
+        updateDisplay(e);
+    });
+});
+
+
+
+
 
 const equal = document.createElement("button");
 equal.textContent = "="
@@ -70,5 +123,15 @@ opContainer.appendChild(equal);
 const clear = document.createElement("button");
 clear.textContent = "C";
 opContainer.appendChild(clear);
+
+clear.addEventListener("click", () => {
+    displayNum1 = "";
+    displayNum2 = "";
+    displayOperator = "";
+    displayFunction.textContent = "0";
+});
+
+
+
 
 
